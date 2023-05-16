ec92f07d02ea1b929fb7a3999b2bcd709f469461
Cherubini <127298157+Cherubini@users.noreply.github.com>
2023-05-16 10:04:35 +0200

    front end alpha

--- service/users-service.js
+++ service/users-service.js
@@ -6,4 +6,9 @@ class UsersService{
         return fetch(this.BASE_URL).then(resp => resp.json());
     };
 
+    static postUser(user){
+        const jsonUser = JSON.stringify(user);
+        return fetch(this.DATA,{method: 'POST', headers: {'content-type':'application/json'}, body: jsonUser})
+    }
+
 }
